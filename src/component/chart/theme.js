2871c3a2d0894bcc5c5a0fbde6869e02d8f9abb6
JW Kim <dasider41@gmail.com>
2016-10-26 16:59:45 +1300

    Added ART and D3 chart.

---
+++ src/component/chart/theme.js
@@ -0,0 +1,11 @@
+//@flow
+'use strict'
+
+export default {
+
+  colors: [
+    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+    "#7f7f7f", "#8c564b", "#e377c2", "#bcbd22", "#17becf"
+  ]
+
+}
